d07b1edf124baba7d507c08b6786b0d7ff6159a2
Juan Jiménez <nefioxdev@gmail.com>
2021-12-19 18:21:13 -0600

    Path operations structure users

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from uuid import UUID
 from datetime import date
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 # Pydantic
 from pydantic import BaseModel
@@ -12,6 +12,7 @@ from pydantic import Field
 
 # FastAPI
 from fastapi import FastAPI
+from fastapi import status
 
 app = FastAPI()
 
@@ -44,12 +45,80 @@ class Tweet(BaseModel):
     content: str = Field(
         ...,
         min_length=1,
-        max_length=256
+        max_length=280
     )
     created_at: datetime = Field(default=datetime.now())
     updated_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 
+# Path Operations
+
 @app.get(path='/')
 def home():
     return {'Twitter API': 'Working!'}
+
+## Users
+
+@app.post(
+    path='/signup',
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary='Register an User',
+    tags=['Users']
+)
+def signup():
+    pass
+
+@app.post(
+    path='/login',
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary='Login an User',
+    tags=['Users']
+)
+def login():
+    pass
+
+@app.get(
+    path='/users',
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary='Show all users',
+    tags=['Users']
+)
+def show_all_users():
+    pass
+
+@app.get(
+    path='/users/{user_id}',
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary='Show an User',
+    tags=['Users']
+)
+def show_an_user():
+    pass
+
+@app.delete(
+    path='/users/{user_id}/delete',
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary='Delete an User',
+    tags=['Users']
+)
+def delete_an_user():
+    pass
+
+@app.put(
+    path='/users/{user_id}/update',
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary='Update an User',
+    tags=['Users']
+)
+def update_an_user():
+    pass
+
+
+
+## Tweets
