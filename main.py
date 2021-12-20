ca0aa80d6457b8a04d14c111b68da1803c573e85
Juan Jiménez <nefioxdev@gmail.com>
2021-12-19 23:03:04 -0600

    Users register

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Python
+import json
 from uuid import UUID
 from datetime import date
 from datetime import datetime
@@ -13,6 +14,7 @@ from pydantic import Field
 # FastAPI
 from fastapi import FastAPI
 from fastapi import status
+from fastapi import Body
 
 app = FastAPI()
 
@@ -69,25 +71,33 @@ class Tweet(BaseModel):
     summary='Register an User',
     tags=['Users']
 )
-def signup():
+def signup(user: UserRegister = Body(...)):
     '''
     Signup
 
     This path operation registers a user in the app
     
     Parameters:
-        - Request Body parameter
-            - user: UserRegister
+    - Request Body parameter
+        - user: UserRegister
     
-    Returns a JSON with the basic user information
-        - user_id: UUID
-        - email: EmailStr
-        - password: str
-        - first_name: str
-        - last_name: str
-        - birth_date: str
+    Returns a JSON with the basic user information:
+    - user_id: UUID
+    - email: EmailStr
+    - password: str
+    - first_name: str
+    - last_name: str
+    - birth_date: datetime
     '''
-    
+    with open('users.json', 'r+', encoding='utf-8') as f:
+        results = json.loads(f.read())
+        user_dict = user.dict()
+        user_dict['user_id'] = str(user_dict['user_id'])
+        user_dict['birth_date'] = str(user_dict['birth_date'])
+        results.append(user_dict)
+        f.seek(0) # Es para escribir sobre la misma lista, es decir, va ir al principio de la lista para que no se hagan más listas cada vez que se escriba sobre el archivo.
+        f.write(json.dumps(results)) # Convierte la lista de diccionarios a JSON.
+        return user
 
 ### Login an user
 @app.post(
